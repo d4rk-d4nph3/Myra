5e4e67205bdeac2411d3acb677ac144aec9ca8c8
bh4b3sh <bhabeshraj678@gmail.com>
2020-04-18 12:45:45 +0545

    Modified plot_ts interface

--- myra.py
+++ myra.py
@@ -8,34 +8,41 @@ from matplotlib.dates import date2num
 from scapy.all import *
 
 
-def generate_summary(input_pcap_file):
-    packets = rdpcap(input_pcap_file)   # Read PCAP file. 
-    print(packets.summary())
-
 def bootstrap():
     os.system('python3 {} {} {} bstrap > {}'.format(
                                 script_name, input_pcap_file, 
                                 output_summary_file, output_summary_file))
 
-def plot_ts(ts_data):
+def generate_summary(input_pcap_file):
+    packets = rdpcap(input_pcap_file)   # Read PCAP file. 
+    print(packets.summary())
+
+def plot_ts(ts_data, title):
     dates = date2num(ts_data)
     plt.plot_date(
         dates, [1]*len(dates), marker="|", markersize=150)
     plt.ylim(0.97,1.2)
+    plt.title(title)
+    plt.yticks([])
+
     plt.show()
     # I know this is cheating. But seems to be the only way.
-    
+
 def dns_report(packets):
     query_count = 0
+    dns_req_ts = []
     for packet in packets:
         if packet.haslayer(DNSRR):
             # Filtering packets with a DNS Round Robin layer. 
             if isinstance(packet.an, DNSRR):
                 query_count += 1
+                packet_ts = datetime.fromtimestamp(packet.time)
+                dns_req_ts.append(packet_ts)
                 print(packet.an.rrname.decode().strip('.')) 
                 # Converting to unicode and stripping the root '.'
 
     print('\nTotal number of DNS Queries made is ' + str(query_count) + '\n')
+    plot_ts(dns_req_ts, 'DNS Flow')
 
 def arp_report(packets):
     arp_count = 0
@@ -60,34 +67,31 @@ def arp_report(packets):
     unique_src_arp_mac = set(src_arp_mac)
     unique_src_arp_ip = set(src_arp_ip)
     unique_req_arp_ip = set(req_arp_ip)
-    plot_ts(req_arp_ts)
-    # req_arp_ip_dist = Counter(req_arp_ip)
-    # src_arp_ip_dist = Counter(src_arp_ip)
-    # src_arp_mac_dist = Counter(src_arp_mac)
-    # print(req_arp_ip_dist)
-    # print(src_arp_ip_dist)
-    # print(src_arp_mac_dist)
-    # print(req_arp_ts)
-    # plt.eventplot(req_arp_ts, lineoffset=0, color='g', linewidths=0.7)
-    # plt.ylim(0,1)
-    # plt.tick_params(labelbottom=False)
-    # dates = date2num(req_arp_ts)
-    # plt.plot_date(dates, [1]*len(dates), marker="|", markersize=150)
-    # plt.ylim(0.97,1.2)
-    # plt.show()
+    req_arp_ip_dist = Counter(req_arp_ip)
+    src_arp_ip_dist = Counter(src_arp_ip)
+    src_arp_mac_dist = Counter(src_arp_mac)
+    print(req_arp_ip_dist)
+    print(src_arp_ip_dist)
+    print(src_arp_mac_dist)
+    print(req_arp_ts)
+    
+    plot_ts(req_arp_ts, 'ARP Flow')
 
 def ip_report(packets):
+    ip_ts = []
     src_ip = []
     dst_ip = []
     unique_src_ip = []
     unique_dst_ip = []
     unique_src_ip_count = 0
     unique_dst_ip_count = 0
-
+    
     for packet in packets:
         if packet.haslayer(IP):
             src_ip.append(packet[IP].src)
             dst_ip.append(packet[IP].dst)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            ip_ts.append(packet_ts)
 
     unique_src_ip = set(src_ip)
     unique_dst_ip = set(dst_ip)
@@ -99,14 +103,21 @@ def ip_report(packets):
     unique_src_ip_count = len(unique_src_ip)
     unique_dst_ip_count = len(unique_dst_ip)
 
-    print('The number of unique source ips is ' + str(unique_src_ip_count) + '\n')
-    print('The number of unique destination ips is ' + str(unique_dst_ip_count) + '\n')
+    print('The number of unique source ips is ' 
+                    + str(unique_src_ip_count) + '\n')
+    print('The number of unique destination ips is ' 
+                    + str(unique_dst_ip_count) + '\n')
+
+    
+    plot_ts(ip_ts, 'IP Flow')
 
 def transport_report(packets):
     tcp_src_port = []
     tcp_dst_port = []
     udp_src_port = []
     udp_dst_port = []
+    tcp_ts = []
+    udp_ts = []
     unique_tcp_src_port = {}
     unique_tcp_dst_port = {}
     unique_udp_src_port = {}
@@ -123,11 +134,15 @@ def transport_report(packets):
             tcp_count += 1
             tcp_src_port.append(packet[IP].sport)
             tcp_dst_port.append(packet[IP].dport)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            tcp_ts.append(packet_ts)
 
         elif packet.haslayer(UDP):
             udp_count += 1
             udp_src_port.append(packet[IP].sport)
             udp_dst_port.append(packet[IP].dport)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            udp_ts.append(packet_ts)
 
     unique_tcp_src_port = set(tcp_src_port)
     unique_tcp_dst_port = set(tcp_dst_port)
@@ -151,28 +166,32 @@ def transport_report(packets):
     print('\nV----- Unique UDP Destination Ports -----V\n')
     print(unique_udp_dst_port)
 
+    plot_ts(tcp_ts, 'TCP Flow')
+    plot_ts(udp_ts, 'UDP Flow')
+
 def main():
     print('<<<<<<<<<< Initialization Completed >>>>>>>>>>\n')
 
-    # print('Initiating Bootstraping Process to Dump Summary Report........\n')
-    # bootstrap()
-    # print('<<<<<<<<<< Bootstrapping Process Completed >>>>>>>>>>\n')
-    # print('####### Summary of packets has been successfuly written in {} #######\n'.format(output_summary_file))
+    print('Initiating Bootstraping Process to Dump Summary Report........\n')
+    bootstrap()
+    print('<<<<<<<<<< Bootstrapping Process Completed >>>>>>>>>>\n')
+    print('####### Summary of packets has been successfuly written in {} #######\n'.format(output_summary_file))
 
     print('Reading pcap file......')
     packets = rdpcap(input_pcap_file)   # Read PCAP file.
     packet_count = len(packets)
 
-    print('The numbers of packets in this pcap file is ' + str(packet_count) + '\n')
+    print('The numbers of packets in this pcap file is '
+                                 + str(packet_count) + '\n')
 
-    # print('Generating DNS Report.....\n')
-    # dns_report(packets)
+    print('Generating DNS Report.....\n')
+    dns_report(packets)
 
-    # print('Generating IP Layer Report....\n')
-    # ip_report(packets)
+    print('Generating IP Layer Report....\n')
+    ip_report(packets)
 
-    # print('Generating Transport Layer Report....\n')
-    # transport_report(packets)
+    print('Generating Transport Layer Report....\n')
+    transport_report(packets)
 
     print('Generating ARP Report....\n')
     arp_report(packets)
@@ -180,7 +199,7 @@ def main():
 
 if len(sys.argv) not in [3, 4]:
     print('''
-    ****   Usage: python3 myra.py <pcap_file> <summary_output_file> [bstrap]   ****
+****   Usage: python3 myra.py <pcap_file> <summary_output_file> [bstrap]   ****
         ''')
     exit()
 
