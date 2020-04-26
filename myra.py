be8979c569f8e27fcb92e070198d5a95677d7190
bh4b3sh <bhabeshraj678@gmail.com>
2020-04-26 16:56:58 +0545

    Implementation of ZMQ underway.

--- myra.py
+++ myra.py
@@ -1,10 +1,10 @@
 from collections import Counter
 from datetime import datetime, date
 import json
+from  multiprocessing import Process
 import os
 import sys
 
-import animation
 import folium
 from folium.plugins import HeatMap
 from geoip2 import database
@@ -13,30 +13,9 @@ import matplotlib.pyplot as plt
 
 from matplotlib.dates import date2num
 import pandas as pd
+import zmq
 
 from scapy.all import *
-
-
-@animation.wait('Reading pcap file')
-def read_pcap(input_pcap_file):
-    try:
-        packets = rdpcap(input_pcap_file)  
-        return packets
-    except FileNotFoundError:
-        return None
-
-
-def generate_summary(packets, output_file):
-    with open(output_file, 'w') as fp:
-        try:
-            original_stdout = sys.stdout
-            sys.stdout = fp
-            fp.write(packets.summary(       
-                        prn=lambda x: str(x.time) 
-                                       + ' ' + x.summary()))
-            # Modifying lambda function as it didn't return packet time.
-        except TypeError as e:
-            sys.stdout = original_stdout
 
 
 def plot_ts(ts_data, title, color):
@@ -74,7 +53,6 @@ def query_geoip(ip_list):
     return resolved_src_country, resolved_locale
 
 
-@animation.wait('Resolving IP addresses to Location')
 def obtain_geoip_info(src_ip_list, dst_ip_list):
     resolved_src_country = []
     resolved_dst_country = []
@@ -157,168 +135,6 @@ def generate_packet_size_plot(packets):
     plt.show()
     
 
-def dns_report(packets):
-    query_count = 0
-    dns_req_ts = []
-    dns_query = []
-
-    for packet in packets:
-        if packet.haslayer(DNSRR):
-            # Filtering packets with a DNS Round Robin layer. 
-            if isinstance(packet.an, DNSRR):
-                query_count += 1
-                packet_ts = datetime.fromtimestamp(packet.time)
-                dns_req_ts.append(packet_ts)
-                print(packet.an.rrname.decode().strip('.'))
-                dns_query.append(packet.an.rrname.decode().strip('.'))
-                # Converting to unicode and stripping the root '.'
-
-    print('\nTotal number of DNS Queries made is '
-                                     + str(query_count) + '\n')
-    
-    plot_ts(dns_req_ts, 'DNS Flow', '#4d4dff')
-    return dns_query
-
-
-def arp_report(packets):
-    arp_count = 0
-    src_arp_mac = []
-    src_arp_ip = []
-    req_arp_ip = []
-    req_arp_ts = []
-    unique_src_arp_mac = {}
-    unique_src_arp_ip = {}
-    unique_req_arp_ip = {}
-
-    for packet in packets:
-        if packet.haslayer(ARP):
-            arp_count += 1
-            if packet[ARP].op == 1:  # ARP Request
-                src_arp_mac.append(packet[ARP].hwsrc)
-                src_arp_ip.append(packet[ARP].psrc)
-                req_arp_ip.append(packet[ARP].pdst)
-                packet_ts = datetime.fromtimestamp(packet.time)
-                req_arp_ts.append(packet_ts)
-
-    unique_src_arp_mac = set(src_arp_mac)
-    unique_src_arp_ip = set(src_arp_ip)
-    unique_req_arp_ip = set(req_arp_ip)
-    req_arp_ip_dist = Counter(req_arp_ip)
-    src_arp_ip_dist = Counter(src_arp_ip)
-    src_arp_mac_dist = Counter(src_arp_mac)
-    print(req_arp_ip_dist)
-    print(src_arp_ip_dist)
-    print(src_arp_mac_dist)
-    print(req_arp_ts)
-    
-    plot_ts(req_arp_ts, 'ARP Flow', "#c71585")
-
-
-@animation.wait('Generating IP Report')
-def ip_report(packets):
-    ip_ts = []
-    src_ip = []
-    dst_ip = []
-    unique_src_ip = []
-    unique_dst_ip = []
-    unique_src_ip_count = 0
-    unique_dst_ip_count = 0
-    
-    for packet in packets:
-        if packet.haslayer(IP):
-            src_ip.append(packet[IP].src)
-            dst_ip.append(packet[IP].dst)
-            packet_ts = datetime.fromtimestamp(packet.time)
-            ip_ts.append(packet_ts)
-
-    unique_src_ip = set(src_ip)
-    unique_dst_ip = set(dst_ip)
-
-    print('\nV----- Unique Source IPs -----V\n')
-    print(unique_src_ip)
-    print('\nV----- Unique Destination IPs -----V\n')
-    print(unique_dst_ip)
-    unique_src_ip_count = len(unique_src_ip)
-    unique_dst_ip_count = len(unique_dst_ip)
-
-    print('The number of unique source ips is ' 
-                    + str(unique_src_ip_count) + '\n')
-    print('The number of unique destination ips is ' 
-                    + str(unique_dst_ip_count) + '\n')
-
-    
-    (src_country, src_locale, 
-    dst_country, dst_locale) = obtain_geoip_info(src_ip, dst_ip)
-    # plot_ts(ip_ts, 'IP Flow', '#af4bce')
-
-    # generate_choropleth(src_country, 'Source Countries')
-    # generate_choropleth(dst_country, 'Destination Countries')
-    
-    # generate_heatmap(src_locale, 'Source Countries')
-    # generate_heatmap(dst_locale, 'Destination Countries') 
-    # TODO Multiprocess maps creation.
-    return unique_src_ip, unique_dst_ip
-
-
-def transport_report(packets):
-    tcp_src_port = []
-    tcp_dst_port = []
-    udp_src_port = []
-    udp_dst_port = []
-    tcp_ts = []
-    udp_ts = []
-    unique_tcp_src_port = {}
-    unique_tcp_dst_port = {}
-    unique_udp_src_port = {}
-    unique_udp_dst_port = {}
-    tcp_src_port_count = 0
-    tcp_dst_port_count = 0
-    udp_src_port_count = 0
-    udp_dst_port_count = 0
-    tcp_count = 0
-    udp_count = 0
-
-    for packet in packets:
-        if packet.haslayer(TCP):
-            tcp_count += 1
-            tcp_src_port.append(packet[IP].sport)
-            tcp_dst_port.append(packet[IP].dport)
-            packet_ts = datetime.fromtimestamp(packet.time)
-            tcp_ts.append(packet_ts)
-
-        elif packet.haslayer(UDP):
-            udp_count += 1
-            udp_src_port.append(packet[IP].sport)
-            udp_dst_port.append(packet[IP].dport)
-            packet_ts = datetime.fromtimestamp(packet.time)
-            udp_ts.append(packet_ts)
-
-    unique_tcp_src_port = set(tcp_src_port)
-    unique_tcp_dst_port = set(tcp_dst_port)
-    unique_udp_src_port = set(udp_src_port)
-    unique_udp_dst_port = set(udp_dst_port)
-
-    tcp_src_port_count = len(unique_tcp_src_port)
-    tcp_dst_port_count = len(unique_tcp_dst_port)
-    udp_src_port_count = len(unique_udp_src_port)
-    udp_dst_port_count = len(unique_udp_dst_port)
-
-    print('Total TCP packet count is ' + str(tcp_count) + '\n')
-    print('V----- Unique TCP Source Ports -----V\n')
-    print(unique_tcp_src_port)
-    print('\nV----- Unique TCP Destination Ports -----V\n')
-    print(unique_tcp_dst_port)
-    
-    print('\nTotal UDP packet count is ' + str(udp_count) + '\n')
-    print('V----- Unique UDP Source Ports -----V\n')
-    print(unique_udp_src_port)
-    print('\nV----- Unique UDP Destination Ports -----V\n')
-    print(unique_udp_dst_port,'\n')
-
-    plot_ts(tcp_ts, 'TCP Flow', '#bf00ff')
-    plot_ts(udp_ts, 'UDP Flow' , '#6f00ff')
-
-
 def matcher(source_set, blacklist_set):
     match_list = []
 
@@ -399,42 +215,328 @@ def threat_intel(src_ip_set, dst_ip_set, domain_set):
     except FileNotFoundError:
         print('One of the Threat Intel Files doesnot exist!!\n'
               'Threat Intel process is skipped...')
+
+
+def dns_report():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:7777")
+    socket.setsockopt(zmq.SUBSCRIBE, b'packets')
+
+    # Second, synchronize with publisher
+    syncclient = context.socket(zmq.REQ)
+    syncclient.connect('tcp://localhost:6666')
+
+    # send a synchronization request
+    syncclient.send(b'')
+
+    # wait for synchronization reply
+    syncclient.recv()
+
+    topic = socket.recv_string()
+    packets = socket.recv_pyobj()
+
+    query_count = 0
+    dns_req_ts = []
+    dns_query = []
+
+    for packet in packets:
+        if packet.haslayer(DNSRR):
+            # Filtering packets with a DNS Round Robin layer. 
+            if isinstance(packet.an, DNSRR):
+                query_count += 1
+                packet_ts = datetime.fromtimestamp(packet.time)
+                dns_req_ts.append(packet_ts)
+                # print(packet.an.rrname.decode().strip('.'))
+                dns_query.append(packet.an.rrname.decode().strip('.'))
+                # Converting to unicode and stripping the root '.'
+
+    print('\nTotal number of DNS Queries made is '
+                                     + str(query_count) + '\n')
+
+    # plot_ts(dns_req_ts, 'DNS Flow', '#4d4dff')
+    # return dns_query
+
+
+def arp_report():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:7777")
+    socket.setsockopt(zmq.SUBSCRIBE, b'packets')
+
+    # Second, synchronize with publisher
+    syncclient = context.socket(zmq.REQ)
+    syncclient.connect('tcp://localhost:6666')
+
+    # send a synchronization request
+    syncclient.send(b'')
+
+    # wait for synchronization reply
+    syncclient.recv()
+
+    topic = socket.recv_string()
+    packets = socket.recv_pyobj()
+
+    arp_count = 0
+    src_arp_mac = []
+    src_arp_ip = []
+    req_arp_ip = []
+    req_arp_ts = []
+    unique_src_arp_mac = {}
+    unique_src_arp_ip = {}
+    unique_req_arp_ip = {}
+
+    for packet in packets:
+        if packet.haslayer(ARP):
+            arp_count += 1
+            if packet[ARP].op == 1:  # ARP Request
+                src_arp_mac.append(packet[ARP].hwsrc)
+                src_arp_ip.append(packet[ARP].psrc)
+                req_arp_ip.append(packet[ARP].pdst)
+                packet_ts = datetime.fromtimestamp(packet.time)
+                req_arp_ts.append(packet_ts)
+
+    unique_src_arp_mac = set(src_arp_mac)
+    unique_src_arp_ip = set(src_arp_ip)
+    unique_req_arp_ip = set(req_arp_ip)
+    req_arp_ip_dist = Counter(req_arp_ip)
+    src_arp_ip_dist = Counter(src_arp_ip)
+    src_arp_mac_dist = Counter(src_arp_mac)
+    print(req_arp_ip_dist)
+    print(src_arp_ip_dist)
+    print(src_arp_mac_dist)
+    print(req_arp_ts)
+    
+    # plot_ts(req_arp_ts, 'ARP Flow', "#c71585")
+
+
+def ip_report():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:7777")
+    socket.setsockopt(zmq.SUBSCRIBE, b'packets')
+
+    # Second, synchronize with publisher
+    syncclient = context.socket(zmq.REQ)
+    syncclient.connect('tcp://localhost:6666')
+
+    # send a synchronization request
+    syncclient.send(b'')
+
+    # wait for synchronization reply
+    syncclient.recv()
+
+    topic = socket.recv_string()
+    packets = socket.recv_pyobj()
+
+    ip_ts = []
+    src_ip = []
+    dst_ip = []
+    unique_src_ip = []
+    unique_dst_ip = []
+    unique_src_ip_count = 0
+    unique_dst_ip_count = 0
+    
+    for packet in packets:
+        if packet.haslayer(IP):
+            src_ip.append(packet[IP].src)
+            dst_ip.append(packet[IP].dst)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            ip_ts.append(packet_ts)
+
+    unique_src_ip = set(src_ip)
+    unique_dst_ip = set(dst_ip)
+
+    print('\nV----- Unique Source IPs -----V\n')
+    print(unique_src_ip)
+    print('\nV----- Unique Destination IPs -----V\n')
+    print(unique_dst_ip)
+    unique_src_ip_count = len(unique_src_ip)
+    unique_dst_ip_count = len(unique_dst_ip)
+
+    print('The number of unique source ips is ' 
+                    + str(unique_src_ip_count) + '\n')
+    print('The number of unique destination ips is ' 
+                    + str(unique_dst_ip_count) + '\n')
+
+    
+    # (src_country, src_locale, 
+    # dst_country, dst_locale) = obtain_geoip_info(src_ip, dst_ip)
+    # plot_ts(ip_ts, 'IP Flow', '#af4bce')
+
+    # generate_choropleth(src_country, 'Source Countries')
+    # generate_choropleth(dst_country, 'Destination Countries')
+    
+    # generate_heatmap(src_locale, 'Source Countries')
+    # generate_heatmap(dst_locale, 'Destination Countries') 
+    # TODO Multiprocess maps creation.
+    # return unique_src_ip, unique_dst_ip
+
+
+def transport_report():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:7777")
+    socket.setsockopt(zmq.SUBSCRIBE, b'packets')
+
+    # Second, synchronize with publisher
+    syncclient = context.socket(zmq.REQ)
+    syncclient.connect('tcp://localhost:6666')
+
+    # send a synchronization request
+    syncclient.send(b'')
+
+    # wait for synchronization reply
+    syncclient.recv()
+
+    topic = socket.recv_string()
+    packets = socket.recv_pyobj()
+
+    tcp_src_port = []
+    tcp_dst_port = []
+    udp_src_port = []
+    udp_dst_port = []
+    tcp_ts = []
+    udp_ts = []
+    unique_tcp_src_port = {}
+    unique_tcp_dst_port = {}
+    unique_udp_src_port = {}
+    unique_udp_dst_port = {}
+    tcp_src_port_count = 0
+    tcp_dst_port_count = 0
+    udp_src_port_count = 0
+    udp_dst_port_count = 0
+    tcp_count = 0
+    udp_count = 0
+
+    for packet in packets:
+        if packet.haslayer(TCP):
+            tcp_count += 1
+            tcp_src_port.append(packet[IP].sport)
+            tcp_dst_port.append(packet[IP].dport)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            tcp_ts.append(packet_ts)
+
+        elif packet.haslayer(UDP):
+            udp_count += 1
+            udp_src_port.append(packet[IP].sport)
+            udp_dst_port.append(packet[IP].dport)
+            packet_ts = datetime.fromtimestamp(packet.time)
+            udp_ts.append(packet_ts)
+
+    unique_tcp_src_port = set(tcp_src_port)
+    unique_tcp_dst_port = set(tcp_dst_port)
+    unique_udp_src_port = set(udp_src_port)
+    unique_udp_dst_port = set(udp_dst_port)
+
+    tcp_src_port_count = len(unique_tcp_src_port)
+    tcp_dst_port_count = len(unique_tcp_dst_port)
+    udp_src_port_count = len(unique_udp_src_port)
+    udp_dst_port_count = len(unique_udp_dst_port)
+
+    print('Total TCP packet count is ' + str(tcp_count) + '\n')
+    print('V----- Unique TCP Source Ports -----V\n')
+    print(unique_tcp_src_port)
+    print('\nV----- Unique TCP Destination Ports -----V\n')
+    print(unique_tcp_dst_port)
+    
+    print('\nTotal UDP packet count is ' + str(udp_count) + '\n')
+    print('V----- Unique UDP Source Ports -----V\n')
+    print(unique_udp_src_port)
+    print('\nV----- Unique UDP Destination Ports -----V\n')
+    print(unique_udp_dst_port,'\n')
+
+    # plot_ts(tcp_ts, 'TCP Flow', '#bf00ff')
+    # plot_ts(udp_ts, 'UDP Flow' , '#6f00ff')
+
+
+def pcap_reader():
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+    # set SNDHWM, so we don't drop messages for slow subscribers
+    publisher.sndhwm = 1100000
+    publisher.bind("tcp://*:7777")  # * 7xxx are data channels.
+
+    # Socket to receive signals
+    syncservice = context.socket(zmq.REP)
+    syncservice.bind('tcp://*:6666')  # * 6xxx are sync channels.
+    # Get synchronization from subscribers
+
+    SUBSCRIBERS_EXPECTED = 5
+    subscribers = 0
+    while subscribers < SUBSCRIBERS_EXPECTED:
+        # wait for synchronization request
+        msg = syncservice.recv()
+        # send synchronization reply
+        syncservice.send(b'')
+        subscribers += 1
+        print("+1 subscriber (%i/%i)" % (subscribers, SUBSCRIBERS_EXPECTED))
+
+    try:
+        packets = rdpcap(input_pcap_file)
+    except FileNotFoundError:
+        packets = None
+
+    i = 0
+    topic = 'packets'
+    while i < 1:
+        i += 1
+        publisher.send_string(topic, zmq.SNDMORE)
+        publisher.send_pyobj(packets)
+        print('Sent Packets to Plugins')
     
 
-def main():
-    print('<<<<<<<<<< Initialization Completed >>>>>>>>>>\n')
-    
-    packets = read_pcap(input_pcap_file)
+def generate_summary():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://localhost:7777")
+    socket.setsockopt(zmq.SUBSCRIBE, b'packets')
+
+    # Second, synchronize with publisher
+    syncclient = context.socket(zmq.REQ)
+    syncclient.connect('tcp://localhost:6666')
+
+    # send a synchronization request
+    syncclient.send(b'')
+
+    # wait for synchronization reply
+    syncclient.recv()
+
+    topic = socket.recv_string()
+    packets = socket.recv_pyobj()
+
     if packets is None:
-        print('The input pcap file does not exist!!\n'
-              'Exiting the program...')
+        print('Input pcap file does not exist!!\n'
+              'Exiting...')
         exit()
 
-
-    generate_summary(packets, output_summary_file)
-
-    print('####### Summary of packets has been successfuly written in {}'
-                                  ' #######'.format(output_summary_file))
-
+    with open(output_summary_file, 'w') as fp:
+        try:
+            original_stdout = sys.stdout
+            sys.stdout = fp
+            fp.write(packets.summary(       
+                        prn=lambda x: str(x.time) 
+                                       + ' ' + x.summary()))
+            # Modifying lambda function as it didn't return packet time.
+        except TypeError as e:
+            sys.stdout = original_stdout
+    
     packet_count = len(packets)
     print('\nThe numbers of packets in this pcap file is '
                                  + str(packet_count) + '\n')
-    
-    # generate_packet_size_plot(packets)
-    print('Generating DNS Report.....\n')
-    dns_query_list = dns_report(packets)
+    print('####### Summary of packets has been successfuly written in {}'
+                                  ' #######'.format(output_summary_file))
 
-    print('Generating IP Layer Report....\n')
-    src_ip, dst_ip = ip_report(packets)
+def main():
+    Process(target = pcap_reader).start()
+    Process(target = generate_summary).start()
+    Process(target = dns_report).start()
+    Process(target = arp_report).start()
+    Process(target = ip_report).start()
+    Process(target = transport_report).start()
+    Process(target = threat_intel).start()
 
-    # print('Generating Transport Layer Report....\n')
-    # transport_report(packets)
-
-    # print('Generating ARP Report....\n')
-    # arp_report(packets)
-
-    threat_intel(src_ip, dst_ip, dns_query_list)
-    
+    # # generate_packet_size_plot(packets)    
 
 if len(sys.argv) not in [3, 4]:
     print('''
